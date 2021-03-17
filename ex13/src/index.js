52a27f86e907d57cc4a3ca1ea135bae2cf0bc542
Jasmina Secic <jasmina.secic98@outlook.com>
2021-03-17 03:20:33 +0100

    Radio Buttons and Checkboxes

--- ex13/src/index.js
+++ ex13/src/index.js
@@ -1,11 +1,17 @@
 function inverseWhile() {
     var i = 5;
     var fiveNumbers = "";
+    var counter = 0;
     while (i <= 5 && i >= 0) {
         fiveNumbers += i.toString();
         i--;
+        counter++;
+        if(counter < 6) {
+            fiveNumbers += ",";
+        }
     }
     return fiveNumbers;
 }
 console.log(inverseWhile());
 module.exports=inverseWhile;
+
